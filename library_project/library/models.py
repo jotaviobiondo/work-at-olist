88c45e4a9b6ccfe98d2c9127a102ded5a20194d6
João Otávio Biondo <joao.o.b@hotmail.com>
2020-04-15 19:42:41 -0300

    create Author model

--- library_project/library/models.py
+++ library_project/library/models.py
@@ -1,3 +1,24 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class AbstractBaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Author(AbstractBaseModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'author'
+        verbose_name_plural = 'authors'
+
+    def __str__(self):
+        return self.name
+
