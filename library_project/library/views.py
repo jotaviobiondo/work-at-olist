96c02a2a0b40d6c0468e161f66fc851b3cfece55
João Otávio Biondo <joao.o.b@hotmail.com>
2020-04-15 20:54:55 -0300

    exposing authors data in an endpoint

--- library_project/library/views.py
+++ library_project/library/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Author
+from .serializers import AuthorSerializer
+
+
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
