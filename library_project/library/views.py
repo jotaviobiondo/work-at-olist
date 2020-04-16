c6b8d2011425e35352c19c927bdd65f37182c67e
João Otávio Biondo <joao.o.b@hotmail.com>
2020-04-15 22:40:45 -0300

    adding ordering and search authors by name in the api

--- library_project/library/views.py
+++ library_project/library/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 
+from .filters import AuthorFilter
 from .models import Author
 from .serializers import AuthorSerializer
 
@@ -7,3 +9,7 @@ from .serializers import AuthorSerializer
 class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = AuthorFilter
+    ordering_fields = ['name']
+    ordering = 'name'
