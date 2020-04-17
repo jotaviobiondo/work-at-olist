3eb7482102b6c7027978f98880ea0b1df256caab
João Otávio Biondo <joao.o.b@hotmail.com>
2020-04-16 23:46:33 -0300

    adding tests for Book model

--- library_project/library/tests/test_models.py
+++ library_project/library/tests/test_models.py
@@ -1,7 +1,9 @@
+import datetime
+
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from library.models import Author
+from library.models import Author, Book
 
 
 class AuthorModelTest(TestCase):
@@ -74,3 +76,71 @@ class AuthorModelTest(TestCase):
 
         authors_count_now = Author.objects.all().count()
         self.assertEqual(authors_count_now, self.authors_count_before)
+
+
+class BookModelTest(TestCase):
+    fixtures = ['test_data']
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.books = Book.objects.all()
+        cls.author = Author.objects.first()
+
+    def create_test_book(self, name='Test Book', edition=1, publication_year=2020):
+        book = Book.objects.create(name=name, edition=edition, publication_year=publication_year)
+
+        book.authors.add(self.author)
+
+        return book
+
+    def test_create_valid_book(self):
+        books_count_before = self.books.count()
+
+        self.create_test_book()
+
+        self.assertEqual(self.books.count(), books_count_before + 1)
+
+    def test_create_book_sanitize_name(self):
+        name = '  Test Book  \t'
+        book = self.create_test_book(name=name)
+
+        self.assertEqual(name.strip(), book.name)
+
+    def test_create_book_with_blank_name(self):
+        blank_names = ['', ' ', '\t', '\n', None]
+
+        books_count_before = self.books.count()
+
+        for blank_name in blank_names:
+            with self.subTest(book_name=blank_name):
+                with self.assertRaises(ValidationError):
+                    self.create_test_book(name=blank_name)
+
+        self.assertEqual(self.books.count(), books_count_before)
+
+    def test_create_book_with_invalid_edition(self):
+        invalid_editions = [-1, None]
+
+        books_count_before = self.books.count()
+
+        for invalid_edition in invalid_editions:
+            with self.subTest(book_edition=invalid_edition):
+                with self.assertRaises(ValidationError):
+                    self.create_test_book(edition=invalid_edition)
+
+        self.assertEqual(self.books.count(), books_count_before)
+
+    def test_create_book_with_invalid_publication_year(self):
+        today = datetime.date.today()
+        next_year = today.year + 1
+
+        invalid_years = [-1, next_year, None]
+
+        books_count_before = self.books.count()
+
+        for invalid_year in invalid_years:
+            with self.subTest(book_publication_year=invalid_year):
+                with self.assertRaises(ValidationError):
+                    self.create_test_book(publication_year=invalid_year)
+
+        self.assertEqual(self.books.count(), books_count_before)
