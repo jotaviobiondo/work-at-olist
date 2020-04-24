58b75531550b737d390ee7b9ffe71b38547cee09
João Otávio Biondo <joao.o.b@hotmail.com>
2020-04-24 18:56:27 -0300

    setting up files to deploy on heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn --chdir library_project library_project.wsgi --log-file -
+release: python library_project/manage.py migrate
--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -33,6 +33,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'library_project.urls'
@@ -84,6 +85,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Rest Framework
 REST_FRAMEWORK = {
